a274949a94a6516c4992710bc0a4c440a3e450af
Yasuhiro ISHII <ishii.yasuhiro@gmail.com>
2012-09-15 09:51:28 +0900

    Added PellerduinoCore(creating underway)

---
+++ hardware/arduino/cores/arduino/Pellerduino.h
@@ -0,0 +1,28 @@
+/*
+  Arduino-like platform for Propeller w/propgcc
+
+  Coded by Yasuhiro ISHII 2012
+
+  This software is distributed under Apache2.0 license.
+ */
+
+#ifndef __PELLERDUINO_H__
+#define __PELLERDUINO_H__
+
+#include <propeller.h>
+
+void setup(void);
+void loop(void);
+
+void pinMode(int,int);
+void digitalWrite(int,int);
+int digitalRead(int);
+
+void delay(long);
+
+#define HIGH		1
+#define	LOW		0
+#define	INPUT		0
+#define	OUTPUT		1
+
+#endif /* __PELLERDUINO_H__ */
